2433d5bfe02d4f4e9107093fcda7178dc99fbfdc
Nicholas Pettas <nick@x1.xyz>
2026-02-03 14:04:16 -0800

    Pin Rust version to 1.82.0 for Solana SDK compatibility

--- ci/rust-version.sh
+++ ci/rust-version.sh
@@ -18,7 +18,7 @@
 if [[ -n $RUST_STABLE_VERSION ]]; then
   stable_version="$RUST_STABLE_VERSION"
 else
-  stable_version=1.85.0
+  stable_version=1.82.0
 fi
 
 if [[ -n $RUST_NIGHTLY_VERSION ]]; then
